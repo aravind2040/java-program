ac068d357a208452ba22e19a1679cf1a586b9c09
aravind2040 <itsaravind1433@gmail.com>
2022-09-29 09:17:32 +0530

    first commit

---
+++ aravind.java
@@ -0,0 +1,9 @@
+class aravind{
+ 
+         public static void main(String[]arg){
+      
+              System.out.print("Welcome To aravind");
+              System.out.println("learn more");
+            }
+
+}      
