c33bca07f3413d59d68bd0d330df2a919c56e2b2
aravind2040 <itsaravind1433@gmail.com>
2022-09-30 21:48:37 +0530

    ok

---
+++ first.java
@@ -0,0 +1,10 @@
+
+class aravind{
+ 
+         public static void main(String[]arg){
+              String name ="welcome to aravind java course";
+              System.out.println("Welcome To aravind");
+              System.out.println(name);
+            }
+
+}  
