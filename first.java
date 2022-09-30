2ee0025a4c1dacf4410a408432b86132d7f3e8ab
aravind2040 <113892628+aravind2040@users.noreply.github.com>
2022-09-30 22:12:00 +0530

    Update first.java
    add some message

--- first.java
+++ first.java
@@ -3,6 +3,7 @@ class aravind{
  
          public static void main(String[]arg){
               String name ="welcome to aravind java course";
+               String name ="welcome";
               System.out.println("Welcome To aravind");
               System.out.println(name);
             }
