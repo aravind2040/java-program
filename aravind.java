c049eb210d73793c933ca573329bb76b2c074991
aravind2040 <itsaravind1433@gmail.com>
2022-10-05 20:21:26 +0530

    “update”

--- aravind.java
+++ aravind.java
@@ -1,10 +1,40 @@
 
 class aravind{
  
-         public static void main(String[]arg){
-              String name ="welcome to aravind java course";
-              System.out.println("Welcome To aravind");
-              System.out.println(name);
+              public static void main(String[] arg){
+             String name ="welcome to aravind java course";
+             System.out.println(name);
+
+                //8 for primitive datatype
+               boolean flag = true;
+               System.out.println(flag);    // prints true
+
+                byte range;
+                range = 124;
+                System.out.println(range);    // prints 124
+               
+                 short temperature;
+                 temperature = -200;
+                 System.out.println(temperature);  // prints -200
+
+                 int range1 = -4250000;
+                 System.out.println(range1);  // print -4250000
+
+                            
+                long range2 = -42332200000L;
+                System.out.println(range2);    // prints -42332200000
+
+                 double number1 = -42.3;
+                 System.out.println(number1);  // prints -42.3
+
+                  float number = -42.3f;
+                  System.out.println(number);  // prints -42.3
+
+                   char letter = '\u0051';
+                    char letter1 ='A';
+                    System.out.println(letter1); 
+                  System.out.println(letter);  // prints Q
+
             }
 
 }  
