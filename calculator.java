c9bde8593b18d4c8ab8cc310abe2bd46b67f50fc
aravind2040 <itsaravind1433@gmail.com>
2022-10-06 16:12:00 +0530

    upload

---
+++ calculator.java
@@ -0,0 +1,43 @@
+class calculator{
+
+
+
+       public static void main(String[] args){
+
+         calculator cals= new calculator();
+
+          cals.add();//method call statement
+          cals.subtruct();//method call statement
+          cals.multify();//method call statement
+          cals.division();//method call statement
+                  
+
+  
+      }
+
+     void add(){ // method signature method name
+
+        System.out.println(10+20); /// method definition
+
+        }
+        
+     void subtruct(){  // method signature method name
+
+        System.out.println(10-20);//method definition
+
+        }
+        
+     void multify(){   // method signature method name
+
+        System.out.println(10*20); // method definition
+
+        }
+        
+     void division(){  // method signature method name
+
+        System.out.println(10/20); // method definition
+
+        }
+
+
+   }
