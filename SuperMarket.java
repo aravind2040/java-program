5e351a94a30c954cc99bcd661d23f7e1c8d4a3b9
aravind2040 <113892628+aravind2040@users.noreply.github.com>
2022-10-13 09:48:29 +0530

    Update SuperMarket.java

--- SuperMarket.java
+++ SuperMarket.java
@@ -3,7 +3,7 @@ class SuperMarket
    int price,discount;
    String brand;
    
-  SuperMarket( String brand, int price, int discount) //constructor
+  SuperMarket( String brand, int price, int discount) //create one constructor
     {
         this.brand=brand;
         this.price=price;
@@ -26,6 +26,49 @@ output:
 Customer is buying :PepsodantFor price :30 of discount :5
 Customer is buying :LuxFor price :25 of discount :2
 
+/////////////////////////////////////////////////////////////////////////
+   class SuperMarket
+{
+   int price,discount,tax;
+   String brand;
+   
+  SuperMarket( String brand, int price, int discount) //constructor
+    {
+        this.brand=brand;
+        this.price=price;
+        this.discount=discount;
+    }
+     SuperMarket( String brand, int price) //constructor
+    {
+        this.brand=brand;
+        this.price=price;
+        this.discount=discount;
+    }
+     SuperMarket( String brand, int price, int discount,int tax) //create multi constructor
+    {
+        this.brand=brand;
+        this.price=price;
+        this.discount=discount;
+        this.tax=tax;
+    }
+public static void main(String[] args)
+       {
+       SuperMarket brush= new SuperMarket("Pepsodant",30,5);
+       SuperMarket book= new SuperMarket("book",25);
+         SuperMarket soap= new SuperMarket("Lux",300,20,2);
+       brush.purchase();
+       book.purchase();
+        soap.purchase();
+       }
+  void purchase()
+       {
+         System.out.println("Customer is buying :"+brand+"For price :"+price+"of discount :"+discount+"tax :"+tax);
+       }
+}
+////////////////////////////////////////////////////////////////////////////   
+Customer is buying :PepsodantFor price :30of discount :5tax :0
+Customer is buying :bookFor price :25of discount :0tax :0
+Customer is buying :LuxFor price :300of discount :20tax :2
 /////////////////////////////////////////////////////////////////////////
 public class ClassRoom {
     //global variables 
